611e2d00bffe369361ba24900cbf43a997c8c3b6
KitSymes <kitsymes@btinternet.com>
2022-04-13 16:15:47 +0100

    Week 11 Commit #02
    - Add Branch and Bound Dynamic Solving

--- Y2-FMaA-Assignment-2/Y2-FMaA-Assignment-2.cpp
+++ Y2-FMaA-Assignment-2/Y2-FMaA-Assignment-2.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+void BranchAndBound(string path, int cost, int weight, int currentWaypoint);
 void BruteForce(string path, int cost, int weight, int currentWaypoint);
 
 void SetGraph(int x, int y, int value);
@@ -16,13 +17,24 @@ int _graph[5][5];
 const int _loads[5] = { 20,40,70,10,30 };
 // Fuel Cost Per Tonne Per Million KM
 const int fuelCost = 25;
+
+int _babMin, _babAttempts;
+string _babPath;
 map<string, int> _bruteForce;
 
 int main()
 {
 	DefaultGraph();
 
-	cout << "Brute Forcing" << endl;
+	_babMin = INT_MAX;
+	_babAttempts = 0;
+
+	cout << "Branch and Bound Dynamic Solving" << endl;
+	BranchAndBound("", 0, 0, 0);
+	cout << "Took " << _babAttempts << " combinations" << endl;
+	cout << "Shortest Path is " << _babPath << " which costs " << (fuelCost * _babMin) << " Intergalactic Currency Units" << endl;
+
+	/*cout << "Brute Forcing" << endl;
 	BruteForce("", 0, 0, 0);
 	cout << "Took " << _bruteForce.size() << " combinations" << endl;
 	string shortestPath;
@@ -35,7 +47,38 @@ int main()
 		shortestPath = pair.first;
 		shortestCost = pair.second;
 	}
-	cout << "Shortest Path is " << shortestPath << " which costs " << (fuelCost * shortestCost) << endl;
+	cout << "Shortest Path is " << shortestPath << " which costs " << (fuelCost * shortestCost) << endl;*/
+}
+
+void BranchAndBound(string path, int cost, int weight, int currentWaypoint)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		stringstream id;
+		id << i;
+		if (path.find(id.str()) != std::string::npos)
+			continue;
+
+		int projected = cost + weight * GetGraph(currentWaypoint, i);
+		if (projected >= _babMin)
+		{
+			cout << "Cancelling " << path << " as " << projected << ">=" << _babMin << endl;
+			continue;
+		}
+
+		stringstream newPath;
+		newPath << path << i;
+		// For the first waypoint the weight is 0 so even though GetGraph() will try (0, i) it doesn't affect the results
+		BranchAndBound(newPath.str(), projected, weight + _loads[i], i);
+	}
+	if (path.length() >= 5)
+	{
+		_bruteForce[path] = cost;
+		cout << "Found new min path " << path << " which is " << cost << " Weight*KM units" << endl;
+		_babMin = cost;
+		_babPath = path;
+		_babAttempts++;
+	}
 }
 
 void BruteForce(string path, int cost, int weight, int currentWaypoint)
@@ -48,6 +91,7 @@ void BruteForce(string path, int cost, int weight, int currentWaypoint)
 			continue;
 		stringstream newPath;
 		newPath << path << i;
+		// For the first waypoint the weight is 0 so even though GetGraph() will try (0, i) it doesn't affect the results
 		BruteForce(newPath.str(), cost + weight * GetGraph(currentWaypoint, i), weight + _loads[i], i);
 	}
 	if (path.length() >= 5)
@@ -62,7 +106,7 @@ void DefaultGraph()
 	SetGraph(0, 1, 10);
 	SetGraph(0, 2, 15);
 	SetGraph(0, 3, 12);
-	SetGraph(0, 4, 20);;
+	SetGraph(0, 4, 20);
 
 	SetGraph(1, 2, 12);
 	SetGraph(1, 3, 25);
